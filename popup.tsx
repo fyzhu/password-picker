fd03871cf78c02b3a10790a9b120e2176b49f4a6
ext.zhuyunfeng1 <i@zhuyunfeng.com>
2023-09-13 14:13:15 +0800

    feat: 🎸 add a button in popup

--- popup.tsx
+++ popup.tsx
@@ -10,17 +10,13 @@ function IndexPopup() {
         flexDirection: "column",
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+      <button onClick={()=>{
+        window.postMessage('test', 'http://example.org:8080')
+        setData('postmessage')
+        
+      }}>pick password</button>
+      <div>{data}</div>
+      
     </div>
   )
 }
